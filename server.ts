4563a8e66949a099d55bda0d5f5bbe2bac4f769a
temlabs <temilabinjo@hotmail.co.uk>
2022-04-28 15:19:53 +0100

    Two new patch routes specified to edit pastes and comments respectively

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import { Client } from "pg";
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
-import {Paste, Comment} from "./interfaces";
+import {Paste, Comment, PasteFullDetails} from "./interfaces";
 
 config(); //Read .env file lines as though they were env vars.
 
@@ -84,6 +84,50 @@ app.post<{paste_id: string},{},Comment>("/pastes/:paste_id/comments", cors(), as
     res.status(404).json(error);
   }
 });
+
+
+app.patch<{paste_id: string},{},{paste_content: string}>("/pastes/:paste_id", cors(), async (req, res) => {
+  const paste_id = req.params.paste_id;
+  console.log(paste_id);
+  try {
+
+    if (!req.body.paste_content) {
+      throw 'Incorrect body format';
+    }
+
+    const query = 'UPDATE pastes SET paste_content = $1 WHERE paste_id = $2';
+    const queryParams = [req.body.paste_content,paste_id]; 
+    
+    const dbres = await client.query(query,queryParams);
+    res.status(201).json(dbres.rows);
+
+  } catch (error) {
+    res.status(404).json(error);
+  }
+});
+
+
+app.patch<{paste_id: string, comment_id: string},{},{comment_content: string}>("/pastes/:paste_id/comments/:comment_id", cors(), async (req, res) => {
+  const paste_id = req.params.paste_id;
+  const comment_id = req.params.comment_id;
+  try {
+
+    if (!req.body.comment_content) {
+      throw 'Incorrect body format';
+    }
+
+    const query = 'UPDATE comments SET comment_content = $1 WHERE paste_id = $2 and comment_id = $3';
+    const queryParams = [req.body.comment_content,paste_id, comment_id]; 
+    
+    const dbres = await client.query(query,queryParams);
+    res.status(201).json(dbres.rows);
+
+  } catch (error) {
+    res.status(404).json(error);
+  }
+});
+
+
 
 
 app.get<{paste_id: string}>("/pastes/:paste_id/comments", cors(), async (req, res) => {
